86297689423f8a6d9cf75bf5f701aceb9cd87083
NogNoa <nngnnadas@gmail.com>
2021-01-31 18:10:17 +0200

    preventing -1

--- 1-bit.py
+++ 1-bit.py
@@ -4,13 +4,17 @@ PC = 0
 
 
 def nand(call, operand, back):
+    global PC
     data[back] = int(not (data[call] and operand))
+    PC += 1
 
 
 def ifeq0(call, instruct):
     global PC
     if not data[call]:
-        PC = instruct - 1
+        PC = instruct
+    else:
+        PC += 1
 
 
 while PC < 5:
@@ -19,5 +23,7 @@ while PC < 5:
         nand(call=PC4 + 1, operand=PC4 + 2, back=PC4 + 3)
     else:
         ifeq0(call=PC4 + 1, instruct=PC4 + 2 + 2 * (PC4 + 3))
-    PC += 1
 print(data)
+
+# 5 is fine, in hardware it whold notice a roll over, that's why I can't allow -1 to happen, and so incrementing
+# the PC appears twice when it could otherwisw have appeared once.
