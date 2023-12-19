5299f9f4c90b73e556f774f9287dd7f92c1af2ca
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-12-19 13:11:51 +0200

    test magnitude, add  sub

--- test_abacus.py
+++ test_abacus.py
@@ -1,3 +1,5 @@
+import math
+
 import Abacus as aba
 import random
 import unittest
@@ -16,6 +18,12 @@ class MyTestCase(unittest.TestCase):
         abacus.load(numb)
         self.assertEqual(numb, int(abacus))
 
+    def test_magnitude(self):
+        numb = random.randint(0, P24_6)
+        abacus._fast_load(numb)
+        expected = math.ceil(math.log(numb, 24))
+        self.assertEqual(expected, abacus.magnitude())
+
     def test_right(self):
         numb = random.randint(0, P24_6)
         abacus._fast_load(numb)
@@ -31,6 +39,20 @@ class MyTestCase(unittest.TestCase):
         result = int(abacus)
         self.assertEqual(numb * 24 % (P24_6 + 1), result)
         self.assertEqual(numb * 24, result + rem * (P24_6 + 1))
+
+    def test_add(self):
+        a, b = (random.randint(0, P24_6) for _ in range(2))
+        if b > a: a, b = b, a
+        abacus._fast_load(a)
+        human.add1(b)
+        self.assertEqual(a + b, int(abacus))
+
+    def test_sub(self):
+        a, b = (random.randint(0, P24_6) for _ in range(2))
+        if b > a: a, b = b, a
+        abacus._fast_load(a)
+        human.sub1(b)
+        self.assertEqual(a - b, int(abacus))
 
     def test_mult(self):
         a, b = (random.randint(0, P24_3) for _ in range(2))
